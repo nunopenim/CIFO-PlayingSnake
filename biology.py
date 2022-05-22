aaeffa79aad033d979a4afe19bf890d74a985809
nunopenim <nuno.penim@gmail.com>
2022-05-22 15:23:52 +0100

    biology: idk, breeding is a total roulette

--- biology.py
+++ biology.py
@@ -1,7 +1,8 @@
 import numpy as np
 from play_the_game import machine_play
 from Snake_Game import display, clock
-from random import choice, randint, uniform
+from random import randint
+
 
 class NN:
     in_layer = 5
@@ -12,7 +13,6 @@ class NN:
     weight1_shape = (25, 5)
     weight2_shape = (10, 25)
     weight3_shape = (1, 10)
-
 
     # Auxiliary
     @staticmethod
@@ -44,54 +44,67 @@ class NN:
 
         return activation3
 
-class GeneticAlg:
 
+class GeneticAlg:
     # Compute Fitness
     @staticmethod
     def calc_fitness(population):
         fitnesses = []
         for i in range(population.shape[0]):
             fitness = machine_play(display, clock, population[i])
-            print('Chromossome number ' + str(i) + "fitness: " + str(fitness))
+            # print('chromosome: ' + str(i) + ", fitness: " + str(fitness))
             fitnesses.append(fitness)
         return np.array(fitnesses)
 
     # Selection
     @staticmethod
-    def natural_selection(pop, fitness, nparents):
-        parents = np.empty((nparents, pop.shape[1]))
+    def natural_selection(population, fitness, nparents):
+        parents = np.empty((nparents, population.shape[1]))
+
+        # basically we choose the best nparents from our population and return an numpy array of them
         for parent_num in range(nparents):
             max_fitness_idx = np.where(fitness == np.max(fitness))[0][0]
-            parents[parent_num, :] = pop[max_fitness_idx, :]
+            parents[parent_num, :] = population[max_fitness_idx, :]
             fitness[max_fitness_idx] = int(float('-Inf'))
         return np.array(parents)
 
     # Crossover
     @staticmethod
     def breeding(parents, size):
-        offspring = np.empty(size)
-        for k in range(size[0]):
-            while True:
-                parent1_idx = randint(0, parents.shape[0] - 1)
-                parent2_idx = randint(0, parents.shape[0] - 1)
-                # produce offspring from two parents if they are different
-                if parent1_idx != parent2_idx:
-                    for j in range(size[1]):
-                        if uniform(0, 1) < 0.5:
-                            offspring[k, j] = parents[parent1_idx, j]
-                        else:
-                            offspring[k, j] = parents[parent2_idx, j]
-                    break
+        offspring_size = size[0]
+        weights = size[1]
+        offspring = np.empty(offspring_size)
+        for k in range(size):
+
+            # We get 2 random different parents
+            parent1 = randint(0, parents.shape[0] - 1)
+            parent2 = randint(0, parents.shape[0] - 1)
+            # This is a safeguard so that they don't breed with themselves, because... duh?
+            while parent1 == parent2:
+                parent1 = randint(0, parents.shape[0] - 1)
+                parent2 = randint(0, parents.shape[0] - 1)
+
+            # We make the crossover, randomly, based on RNG
+            for j in range(weights):
+                # Basically, here I am choosing to get a random number with equal probabilities, then if lower
+                # than half, we choose the chromosome from the first parent, otherwise the second parent.
+                if randint(0, 1000) < 500:
+                    offspring[k, j] = parents[parent1, j]
+                else:
+                    offspring[k, j] = parents[parent2, j]
+
         return np.array(offspring)
 
     # Mutation
     @staticmethod
     def chernobyl(offspring):
         mutated_offspring = offspring.copy()
+
+        # We select a random individual from the pool, then a random chromosome, and mutate it
         for idx in range(offspring.shape[0]):
-            for _ in range(25):
-                i = randint(0, offspring.shape[1] - 1)
+            for i in range(25):
+                rand = randint(0, offspring.shape[1] - 1)
             random_value = np.random.choice(np.arange(-1, 1, step=0.001), size=1, replace=False)
-            mutated_offspring[idx, i] = offspring[idx, i] + random_value
+            mutated_offspring[idx, rand] = offspring[idx, rand] + random_value
         return mutated_offspring
 
