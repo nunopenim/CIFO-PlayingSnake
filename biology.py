68431c53ae506b44778b89845036f06d90af1e2b
nunopenim <nuno.penim@gmail.com>
2022-05-22 16:10:11 +0100

    biology: update on the biology to now include the intelligence

--- biology.py
+++ biology.py
@@ -1,7 +1,9 @@
 import numpy as np
-from play_the_game import machine_play
-from Snake_Game import display, clock
 from random import randint
+
+# Game Stuff
+from Snake_Game import display, clock
+from Snake_Game import starting_positions, blocked_directions, angle_with_apple, generate_button_direction, collision_with_boundaries, collision_with_self, play_game
 
 # This is for crossover tuning adjustments, higher means a finer selection, lower means coarser
 BREEDING_TUNING_VALUE = 100
@@ -119,3 +121,65 @@ class GeneticAlg:
             mutated_offspring[idx, rand] = offspring[idx, rand] + random_value
         return mutated_offspring
 
+
+# Intelligence and fitness function
+def machine_play(display, clock, weights):
+    max_score = 0
+    avg_score = 0
+    test_games = 1
+    score1 = 0
+    steps_per_game = 2500
+    score2 = 0
+
+    for _ in range(test_games):
+        snake_start, snake_position, apple_position, score = starting_positions()
+
+        count_same_direction = 0
+        prev_direction = 0
+
+        for _ in range(steps_per_game):
+            current_direction_vector, is_front_blocked, is_left_blocked, is_right_blocked = blocked_directions(
+                snake_position)
+            angle, snake_direction_vector, apple_direction_vector_normalized, snake_direction_vector_normalized = angle_with_apple(
+                snake_position, apple_position)
+            predictions = []
+            predicted_direction = np.argmax(np.array(NN.forward_propagation(np.array(
+                [is_left_blocked, is_front_blocked, is_right_blocked, apple_direction_vector_normalized[0],
+                 snake_direction_vector_normalized[0], apple_direction_vector_normalized[1],
+                 snake_direction_vector_normalized[1]]).reshape(-1, 7), weights))) - 1
+
+            if predicted_direction == prev_direction:
+                count_same_direction += 1
+            else:
+                count_same_direction = 0
+                prev_direction = predicted_direction
+
+            new_direction = np.array(snake_position[0]) - np.array(snake_position[1])
+            if predicted_direction == -1:
+                new_direction = np.array([new_direction[1], -new_direction[0]])
+            if predicted_direction == 1:
+                new_direction = np.array([-new_direction[1], new_direction[0]])
+
+            button_direction = generate_button_direction(new_direction)
+
+            next_step = snake_position[0] + current_direction_vector
+            if collision_with_boundaries(snake_position[0]) == 1 or collision_with_self(next_step.tolist(),
+                                                                                        snake_position) == 1:
+                score1 += -150
+                break
+
+            else:
+                score1 += 0
+
+            snake_position, apple_position, score = play_game(snake_start, snake_position, apple_position,
+                                                              button_direction, score, display, clock)
+
+            if score > max_score:
+                max_score = score
+
+            if count_same_direction > 8 and predicted_direction != 0:
+                score2 -= 1
+            else:
+                score2 += 2
+
+    return score1 + score2 + max_score * 5000
---
+++ main.py
@@ -0,0 +1,44 @@
+from biology import GeneticAlg, NN
+from Snake_Game import *
+
+# Important to remember:
+#  - Get initial population
+#  - Let nature (but not really...) do it's job
+#  - ???
+#  - Hopefully profit
+
+# humans have 23 pairs, right? so total of 46 chromosomes? dont know, I am gonna start with that
+nchromosomes = 46
+nweights = NN.in_layer * NN.hidden1 + NN.hidden1 * NN.hidden2 + NN.hidden2 * NN.out_layer
+
+# population and lifespan
+p_size = (nchromosomes, nweights)
+population = GeneticAlg.generate_random_population(p_size)
+ngenerations = 10
+n_parents_mating = 6  # it should be an even number!
+
+# Order to remember for the nature, as we already have a random population selected before the nature run:
+#  - Get the fitness
+#  - Natural selection, survival of the fittest, or some nazi experiment, no clue (SELECTION)
+#  - Breed them (CROSSOVER)
+#  - Send them to Chernobyl (MUTATION)
+#  - Your new population now has the fittest parents, and their kids, the weaks died
+#  - Rinse and repeat until number of generations has passed
+
+for gen in range(ngenerations):
+    print("Current generation: " + str(gen))
+
+    # Fitness
+    fitness = GeneticAlg.calc_fitness(population)
+    print("Fittest chromosome value: " + np.max(fitness))
+
+    # Get the best parents and breed them
+    parents = GeneticAlg.natural_selection(population, fitness, n_parents_mating)
+    kids = GeneticAlg.breeding(parents, size=(p_size[0] - parents.shape[0], nweights))
+
+    # Mutations
+    mutant_kids = GeneticAlg.chernobyl(kids)
+
+    # Population Update
+    population[0:parents.shape[0], :] = parents
+    population[parents.shape[0]:, :] = mutant_kids
