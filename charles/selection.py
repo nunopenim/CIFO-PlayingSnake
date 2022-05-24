18dfc6e9cb094d14cd5d4b1a5383bb275f87786e
paulo <paulo>
2022-05-24 06:37:42 +0100

    Charles and setting up multiple implementations

---
+++ charles/selection.py
@@ -0,0 +1,54 @@
+from random import uniform, choice
+from operator import attrgetter
+
+
+def fps(population):
+    """Fitness proportionate selection implementation.
+
+    Args:
+        population (Population): The population we want to select from.
+
+    Returns:
+        Individual: selected individual.
+    """
+
+    if population.optim == "max":
+        # Sum total fitness
+        total_fitness = sum([i.fitness for i in population])
+        # Get a 'position' on the wheel
+        spin = uniform(0, total_fitness)
+        position = 0
+        # Find individual in the position of the spin
+        for individual in population:
+            position += individual.fitness
+            if position > spin:
+                return individual
+
+    elif population.optim == "min":
+        raise NotImplementedError
+
+    else:
+        raise Exception("No optimization specified (min or max).")
+
+
+def tournament(population, size=10):
+    """Tournament selection implementation.
+
+    Args:
+        population (Population): The population we want to select from.
+        size (int): Size of the tournament.
+
+    Returns:
+        Individual: Best individual in the tournament.
+    """
+
+    # Select individuals based on tournament size
+    tournament = [choice(population.individuals) for i in range(size)]
+    # Check if the problem is max or min
+    if population.optim == 'max':
+        return max(tournament, key=attrgetter("fitness"))
+    elif population.optim == 'min':
+        return min(tournament, key=attrgetter("fitness"))
+    else:
+        raise Exception("No optimization specified (min or max).")
+
--- main.py
+++ main.py
@@ -1,45 +1,56 @@
 from biology import GeneticAlg, NN
 from Snake_Game import *
 
-# Important to remember:
-#  - Get initial population
-#  - Let nature (but not really...) do it's job
-#  - ???
-#  - Hopefully profit
 
-# humans have 23 pairs, right? so total of 46 chromosomes? dont know, I am gonna start with that
-nchromosomes = 46
-nweights = NN.in_layer * NN.hidden1 + NN.hidden1 * NN.hidden2 + NN.hidden2 * NN.out_layer
+# first version of the snake implementation
+def implementation1():
+    # Important to remember:
+    #  - Get initial population
+    #  - Let nature (but not really...) do it's job
+    #  - ???
+    #  - Hopefully profit
 
-# population and lifespan
-p_size = (nchromosomes, nweights)
-population = GeneticAlg.generate_random_population(p_size)
-ngenerations = 200
-n_parents_mating = 6  # it should be an even number!
+    # humans have 23 pairs, right? so total of 46 chromosomes? dont know, I am gonna start with that
+    nchromosomes = 46
+    nweights = NN.in_layer * NN.hidden1 + NN.hidden1 * NN.hidden2 + NN.hidden2 * NN.out_layer
 
-# Order to remember for the nature, as we already have a random population selected before the nature run:
-#  - Get the fitness
-#  - Natural selection, survival of the fittest, or some nazi experiment, no clue (SELECTION)
-#  - Breed them (CROSSOVER)
-#  - Send them to Chernobyl (MUTATION)
-#  - Your new population now has the fittest parents, and their kids, the weaks died
-#  - Rinse and repeat until number of generations has passed
+    # population and lifespan
+    p_size = (nchromosomes, nweights)
+    population = GeneticAlg.generate_random_population(p_size)
+    ngenerations = 200
+    n_parents_mating = 6  # it should be an even number!
 
-for gen in range(ngenerations):
-    print("Current generation: " + str(gen))
+    # Order to remember for the nature, as we already have a random population selected before the nature run:
+    #  - Get the fitness
+    #  - Natural selection, survival of the fittest, or some nazi experiment, no clue (SELECTION)
+    #  - Breed them (CROSSOVER)
+    #  - Send them to Chernobyl (MUTATION)
+    #  - Your new population now has the fittest parents, and their kids, the weaks died
+    #  - Rinse and repeat until number of generations has passed
 
-    # Fitness
-    fitness = GeneticAlg.calc_fitness(population)
-    print("Fittest chromosome value: " + str(np.max(fitness)))
+    for gen in range(ngenerations):
+        print("Current generation: " + str(gen))
 
-    # Get the best parents and breed them
-    parents = GeneticAlg.natural_selection(population, fitness, n_parents_mating)
-    kids = GeneticAlg.breeding(parents, size=(p_size[0] - parents.shape[0], nweights))
+        # Fitness
+        fitness = GeneticAlg.calc_fitness(population)
+        print("Fittest chromosome value: " + str(np.max(fitness)))
 
-    # Mutations
-    mutant_kids = GeneticAlg.chernobyl(kids)
+        # Get the best parents and breed them
+        parents = GeneticAlg.natural_selection(population, fitness, n_parents_mating)
+        kids = GeneticAlg.breeding(parents, size=(p_size[0] - parents.shape[0], nweights))
 
-    # Population Update
-    population[0:parents.shape[0], :] = parents
-    population[parents.shape[0]:, :] = mutant_kids
+        # Mutations
+        mutant_kids = GeneticAlg.chernobyl(kids)
 
+        # Population Update
+        population[0:parents.shape[0], :] = parents
+        population[parents.shape[0]:, :] = mutant_kids
+
+
+def implementation2():
+    pass
+
+
+# UNCOMMENT WHICH VERSION YOU WANT TO RUN
+implementation1()
+#implementation2()
