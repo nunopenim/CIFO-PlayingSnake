919a90d0dd473c2f2b1008f74ac9e3ddf5fe7fbd
paulo <paulo>
2022-05-25 10:59:24 +0100

    slower but smarter

--- biology.py
+++ biology.py
@@ -139,6 +139,7 @@ def machine_play(display, clock, weights):
     avg_score = 0
     test_games = 1
     frame_score = 0
+    steps_per_game = 5000000
     noApple = 0
 
     for _ in range(test_games):
@@ -148,46 +149,57 @@ def machine_play(display, clock, weights):
         count_same_direction = 0
         prev_direction = 0
 
-        current_direction_vector, is_front_blocked, is_left_blocked, is_right_blocked = blocked_directions(
-            snake_position)
-        angle, snake_direction_vector, apple_direction_vector_normalized, snake_direction_vector_normalized = angle_with_apple(
-            snake_position, apple_position)
-        predictions = []
-        predicted_direction = np.argmax(np.array(NN.forward_propagation(np.array(
-            [is_left_blocked, is_front_blocked, is_right_blocked, apple_direction_vector_normalized[0],
-             snake_direction_vector_normalized[0], apple_direction_vector_normalized[1],
-             snake_direction_vector_normalized[1]]).reshape(-1, 7), weights))) - 1
-        if predicted_direction == prev_direction:
-            count_same_direction += 1
-        else:
-            count_same_direction = 0
-            prev_direction = predicted_direction
-        new_direction = np.array(snake_position[0]) - np.array(snake_position[1])
-        if predicted_direction == -1:
-            new_direction = np.array([new_direction[1], -new_direction[0]])
-        if predicted_direction == 1:
-            new_direction = np.array([-new_direction[1], new_direction[0]])
-        button_direction = generate_button_direction(new_direction)
-        next_step = snake_position[0] + current_direction_vector
-        if collision_with_boundaries(snake_position[0]) == 1 or collision_with_self(next_step.tolist(),
-                                                                                    snake_position) == 1:
-            break
-        else:
-            frame_score += 1
-        snake_position, apple_position, score = play_game(snake_start, snake_position, apple_position,
-                                                          button_direction, score, display, clock)
-        if score > max_score:
-            max_score = score
-            noApple = 0
-        else:
-            noApple += 1
-        # STOP IF NO APPLE FOR * TURNS
-        if noApple >= 2500:
-            break
-        if count_same_direction > 8 and predicted_direction != 0:
-            pass
-        else:
-            pass
+
+        for _ in range(steps_per_game):
+            current_direction_vector, is_front_blocked, is_left_blocked, is_right_blocked = blocked_directions(
+                snake_position)
+            angle, snake_direction_vector, apple_direction_vector_normalized, snake_direction_vector_normalized = angle_with_apple(
+                snake_position, apple_position)
+            predictions = []
+            predicted_direction = np.argmax(np.array(NN.forward_propagation(np.array(
+                [is_left_blocked, is_front_blocked, is_right_blocked, apple_direction_vector_normalized[0],
+                 snake_direction_vector_normalized[0], apple_direction_vector_normalized[1],
+                 snake_direction_vector_normalized[1]]).reshape(-1, 7), weights))) - 1
+
+            if predicted_direction == prev_direction:
+                count_same_direction += 1
+            else:
+                count_same_direction = 0
+                prev_direction = predicted_direction
+
+            new_direction = np.array(snake_position[0]) - np.array(snake_position[1])
+            if predicted_direction == -1:
+                new_direction = np.array([new_direction[1], -new_direction[0]])
+            if predicted_direction == 1:
+                new_direction = np.array([-new_direction[1], new_direction[0]])
+
+            button_direction = generate_button_direction(new_direction)
+
+            next_step = snake_position[0] + current_direction_vector
+            if collision_with_boundaries(snake_position[0]) == 1 or collision_with_self(next_step.tolist(),
+                                                                                        snake_position) == 1:
+                break
+
+            else:
+                frame_score += 1
+
+            snake_position, apple_position, score = play_game(snake_start, snake_position, apple_position,
+                                                              button_direction, score, display, clock)
+
+            if score > max_score:
+                max_score = score
+                noApple = 0
+            else:
+                noApple += 1
+
+            # STOP IF NO APPLE FOR * TURNS
+            if noApple >= 2500:
+                break
+
+            if count_same_direction > 8 and predicted_direction != 0:
+                pass
+            else:
+                pass
 
     # Subtract the number of frames since the last fruit was eaten from the fitness
     # This is to discourage snakes from trying to gain fitness by avoiding fruit
